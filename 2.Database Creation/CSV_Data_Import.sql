eb0537bca1f025de7e43e426a5e51d7218e22211
pau brito <pau.brito@outlook.com>
2025-06-05 20:02:41 -0700

    data import code and  silver tables creation

--- 2.Database Creation/CSV_Data_Import.sql
+++ 2.Database Creation/CSV_Data_Import.sql
@@ -1,27 +1,15 @@
-COPY customer_list
-FROM 'C:\Users\paubr\projects\retail-analysis\Data\Fecom Inc Customer List.csv' 
-WITH (FORMAT csv, HEADER, DELIMITER ',');
+\copy customer_list FROM 'C:\\Users\\paubr\\projects\\retail-analysis\\1.Data loaded\\Fecom Inc Customer List.csv' WITH (FORMAT csv, HEADER, DELIMITER ',');
 
-COPY order_payments
-FROM 'C:\Users\paubr\projects\retail-analysis\Data\Fecom Inc Order Payments.csv' 
-WITH (FORMAT csv, HEADER, DELIMITER ',');
+\copy order_payments FROM 'C:\\Users\\paubr\\projects\\retail-analysis\\1.Data loaded\\Fecom Inc Order Payments.csv' WITH (FORMAT csv, HEADER, DELIMITER ',');
 
-COPY order_reviews
-FROM 'C:\Users\paubr\projects\retail-analysis\Data\Fecom_Inc_Order_Reviews_No_Emojis.csv' 
-WITH (FORMAT csv, HEADER, DELIMITER ',');
+\copy order_reviews FROM 'C:\\Users\\paubr\\projects\\retail-analysis\\1.Data loaded\\Fecom_Inc_Order_Reviews_No_Emojis.csv' WITH (FORMAT csv, HEADER, DELIMITER ',');
 
-COPY geolocations
-FROM 'C:\Users\paubr\projects\retail-analysis\Data\Fecom Inc Geolocations.csv' 
-WITH (FORMAT csv, HEADER, DELIMITER ';');
+\copy geolocations FROM 'C:\\Users\\paubr\\projects\\retail-analysis\\1.Data loaded\\Fecom Inc Geolocations.csv' WITH (FORMAT csv, HEADER, DELIMITER ';');
 
-COPY order_items
-FROM 'C:\Users\paubr\projects\retail-analysis\Data\Fecom Inc Order Items.csv' 
-WITH (FORMAT csv, HEADER, DELIMITER ';');
+\copy order_items FROM 'C:\\Users\\paubr\\projects\\retail-analysis\\1.Data loaded\\Fecom Inc Order Items.csv' WITH (FORMAT csv, HEADER, DELIMITER ';');
 
-COPY orders
-FROM 'C:\Users\paubr\projects\retail-analysis\Data\Fecom Inc Orders.csv' 
-WITH (FORMAT csv, HEADER, DELIMITER ';');
+\copy orders FROM 'C:\\Users\\paubr\\projects\\retail-analysis\\1.Data loaded\\Fecom Inc Orders.csv' WITH (FORMAT csv, HEADER, DELIMITER ';');
 
-COPY products
-FROM 'C:\Users\paubr\projects\retail-analysis\Data\Fecom Inc Products.csv' 
-WITH (FORMAT csv, HEADER, DELIMITER ';');
+\copy products FROM 'C:\\Users\\paubr\\projects\\retail-analysis\\1.Data loaded\\Fecom Inc Products.csv' WITH (FORMAT csv, HEADER, DELIMITER ';');
+
+\copy sellers FROM 'C:\\Users\\paubr\\projects\\retail-analysis\\1.Data loaded\\sellers list.csv' WITH (FORMAT csv, HEADER, DELIMITER ';');
